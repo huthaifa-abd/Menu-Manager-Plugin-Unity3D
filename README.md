3c3462fb0103ff612c0262abd2136cdef5d56d60
huthaifa-abd <huth2004@gmail.com>
2016-06-06 21:13:32 +0300

    [Source] Initial Source Code

---
+++ README.md
@@ -0,0 +1,61 @@
+# Menu Manager Plugin Unity3D
+
+A useful plugin for managing complex GUI menus , to be used in unity engine
+
+  - Load Dynamic Menus At Runtime
+  - Easily Load new scene
+  - Simple navigation between menus
+  - Modular Design
+
+You can also:
+  - Import Custom GUI Themes to suit your needs
+  - The plugin comes with nice example scene
+
+This plugin has been developed to be used in any game to simplify the menu creation process ,  i usually use it with in game menus for my space games .
+
+>The plugin still needs lots of updates and improvements , feel free to contribute to the code and fix bugs if you found any.
+
+### Version
+0.1
+
+### Installation
+
+You can get the plugin using one of the methods below
+
+* Clone the github repo
+    * https://github.com/huthaifa-abd/Menu-Manager-Plugin-Unity3D
+* Download the pre built package using the link below
+    * https://github.com/huthaifa-abd/Menu-Manager-Plugin-Unity3D/blob/master/UnityPackage-v0.1/Menu-Manager-Unity-Package-v0.1.unitypackage
+
+Once done start unity and follow the instruction below ,
+
+* Launch Unity Engine
+
+From the menu:
+* go Assets -> Import Package...
+* From context menu in the Project View
+* enter context menu -> Import Package...
+
+Then, in both cases, find the standard packages:
+* Browser to the path where you cloned or downloaded the package and select it .
+
+You can also navigate to the needed package:
+
+* DbClick the package from Windows Explorer or Finder,
+>(just make sure that you have the target project open or that it was the last opened project)
+
+
+### Development
+
+Want to contribute? Great! Just submit a pull request and ill add it to the project its that easy ;)
+
+### Todos
+
+ - Add more generation tools
+ - Make Real time API
+ - Add Gravity Fields
+
+License
+----
+MIT
+**Free Software, Hell Yeah!**
---
+++ Source/Assets/Plugins/MenuManager/Scripts/Menu_Module.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+public class Menu_Module : MonoBehaviour
+{
+    //Load Default Menu
+    public bool LoadDefaultMenu = false;
+    public bool FadeBackground = false;
+    //Menu Game Objects
+    public GameObject[] Menu_List;
+    public static GameObject CurrentMenu_Handler;
+    // Use this for initialization
+    void Start()
+    {
+        if (LoadDefaultMenu)
+        {
+            //Create main menu
+            if(Menu_List.Length > 0 )
+            OpenMenu(Menu_List[0].name);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Scene Management Functions
+    public void LoadScene(string Name)
+    {
+
+
+        //Start Scene
+        try
+        {
+            SceneManager.LoadScene(Name);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Error : " + ex.Message + " Unable to load scene");
+        }
+    }
+
+    //Application Management Funcitons
+    public void ExitApplication()
+    {
+       Application.Quit();
+    }
+
+    //Menu Management Function
+    public void OpenMenu(string Name)
+    {
+
+
+        //Find Menu in menu list
+        foreach (GameObject menu in Menu_List)
+        {
+            if (menu.name == Name)
+            {
+                //Close Previous Menu
+                CloseMenu();
+
+                //Show new menu
+
+                CurrentMenu_Handler = Instantiate(menu, menu.transform.position, this.transform.localRotation) as GameObject;
+                CurrentMenu_Handler.transform.SetParent(GameObject.Find("Menu-Manager").transform, false);
+            }
+
+        }
+    }
+
+    public void CloseMenu()
+    {
+
+        if (CurrentMenu_Handler != null)
+        {
+
+            Destroy(CurrentMenu_Handler);
+        }
+
+
+    }
+}
